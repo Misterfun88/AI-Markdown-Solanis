47a4e9b975997131c4d831b65f14f55c56b2db89
Misterfun88 <misterfu7@proton.me>
2022-11-09 16:10:48 +0000

    Add debugging

--- src/app/solanized/[username]/settings/page.tsx
+++ src/app/solanized/[username]/settings/page.tsx
@@ -14,4 +14,77 @@ type Props = {};
 export default function SettingsPage({}: Props) {
   return (
     <div className="flex flex-col gap-2">
-      <div class
+      <div className="flex flex-col gap-2">
+        <GradientHeading variant={"default"} size={"xs"}>
+          Configurações da conta
+        </GradientHeading>
+      </div>
+      <Tabs defaultValue="profile">
+        <TabsList className="flex justify-start rounded-tr-3xl">
+          <TabsTrigger value="profile">Perfil</TabsTrigger>
+          <TabsTrigger value="notifications">Notificações</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile" className="p-4">
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col gap-4">
+              <h4 className="text-xl font-bold">Customize seu perfil</h4>
+              <p className="text-sm text-muted-foreground w-1/2">
+                Customize seu perfil, atualizando seu nome, sua foto e seu url
+                para compartilhar sua anotação.
+              </p>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger>
+                  <label
+                    className="avatar rounded-full min-h-12 min-w-12 border flex justify-center items-center cursor-pointer"
+                    htmlFor="upload"
+                  >
+                    <input type="file" className="hidden" id="upload" />
+                    <span className="font-[700]">GR</span>
+                  </label>
+                </TooltipTrigger>
+                <TooltipContent>Trocar de foto</TooltipContent>
+              </Tooltip>
+              <div className="flex flex-col gap-4">
+                <Input value={"Gustavo Reis Teuber"} />
+                <div className="flex flex-row w-max h-9">
+                  <div className="p-1 bg-muted rounded-s-md text-muted-foreground flex items-center">
+                    <span className="text-xs">solanized/</span>
+                  </div>
+                  <Input className="h-9 border border-muted rounded-s-none rounded-e-md text-xs p-1" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="notifications" className="p-4">
+          <div className="flex flex-row gap-8">
+            <div className="flex flex-col gap-4 w-1/4">
+              <h4 className="text-xl font-bold">Notificações de perfil</h4>
+              <p className="text-sm text-muted-foreground">
+                Receba notificações quando algo nas suas anotações acontecer.
+              </p>
+            </div>
+            <div className="flex flex-col justify-center gap-4 w-1/2">
+              <div className="flex flex-row gap-2">
+                <Switch />
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm font-bold">Lembretes</p>
+                  <p className="text-sm">
+                    Novidades sobre novas atualizações do Solanis.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-row gap-2">
+                <Switch />
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm font-bold">Menções</p>
+                  <p className="text-sm">
+                    Receber notificações para comentários em suas anotações e
+                    respostas aos seus comentários.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-row gap-2">
+            
