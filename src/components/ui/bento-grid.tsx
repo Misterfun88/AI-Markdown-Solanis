76b532fde19bb2188b82cacda073d10d2ccc2a7e
Misterfun88 <misterfu7@proton.me>
2022-12-02 22:53:19 +0000

    Fix #138

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -45,4 +45,10 @@ export const BentoGridItem = ({
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs da
+        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
