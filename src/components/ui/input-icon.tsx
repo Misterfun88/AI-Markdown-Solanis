35f4d881df622fa87e96f46c8ca2acc61229fe66
Misterfun88 <misterfu7@proton.me>
2022-12-21 09:29:27 +0000

    build version

---
+++ src/components/ui/input-icon.tsx
@@ -0,0 +1,21 @@
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: LucideIcon;
+  endIcon?: LucideIcon;
+}
+
+const InputIcon = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, startIcon, endIcon, ...props }, ref) => {
+    const StartIcon = startIcon;
+    const EndIcon = endIcon;
+
+    return (
+      <div className="w-full relative">
+        <label>
+          {StartIcon && (
