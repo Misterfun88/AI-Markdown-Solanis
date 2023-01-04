17301f500c3f4f2e9563c9bcdea29bf1d0ab522b
Misterfun88 <misterfu7@proton.me>
2023-01-04 09:39:48 +0000

    Created using Colaboratory

---
+++ src/hooks/useAnthropic.ts
@@ -0,0 +1,5 @@
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropicPrompts = {
+  briefing:
+    "You are tasked with creating a comprehensive briefing on a given subject. Your goal is to provide an informative and well-structured overview of the topic, supported by credible source
