f6c733be1110be14f83834301ab79303d5f80dc0
Misterfun88 <misterfu7@proton.me>
2023-01-06 05:40:09 +0000

    New translations en-US.json (Spanish, Peru)

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function ellipsize(str: string, maxLength: number) {
+  if (str.length <= maxLength) return str;
+  return str.slice(0, maxLength) + "...";
+}
